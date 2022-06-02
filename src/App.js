f8975184bb2f4e33f6f29660c775eddf2ed8a81b
janet <janetmutua04@gmail.com>
2022-06-02 12:07:08 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "App.css";
+
+
+const App = () => (
+    <div className="body-color">
+        <h1>Hello Reacter</h1>
+        <iframe src="https://giphy.com/embed/9J7tdYltWyXIY" width="480" height="404" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/internet-google-chrone-9J7tdYltWyXIY">via GIPHY</a></p>
+    </div>
+);
+
+
+export default App;
